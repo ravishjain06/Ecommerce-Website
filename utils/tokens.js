8ff6185c49d7bd214910a37d74f815c74cf29316
Ravish Jain <ravishjain988@gmail.com>
2025-07-19 10:26:49 +0530

    updated token

--- utils/tokens.js
+++ utils/tokens.js
@@ -8,7 +8,7 @@ export function generateAccessToken(user) {
             role: user.role
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '2h' }
+        { expiresIn: '1m' }
     )
 }
 
